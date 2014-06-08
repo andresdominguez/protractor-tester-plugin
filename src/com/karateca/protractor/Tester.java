947a3534ca8420f7efe32c114e765d79bcec9884
andres <code@karateca.com>
2014-06-07 22:52:40 -0400

    use encode

--- src/com/karateca/protractor/Tester.java
+++ src/com/karateca/protractor/Tester.java
@@ -4,19 +4,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class Tester {
+
+  private static String baseUrl = "http://localhost:13000/testSelector?locators=";
+
   public static void testLocator(String locator) throws Exception {
-    URL yahoo = new URL("http://www.yahoo.com/");
-    URLConnection yc = yahoo.openConnection();
+    String loc = "by.model('yourName')";
+
+    String queryString = String.format("{\"popupInput\":\"%s\"}", loc);
+    String encoded = URLEncoder.encode(queryString, "UTF-8");
+    String url = baseUrl + encoded;
+    URLConnection connection = new URL(url).openConnection();
     BufferedReader in = new BufferedReader(
-        new InputStreamReader(
-            yc.getInputStream())
+        new InputStreamReader(connection.getInputStream())
     );
     String inputLine;
 
-    while ((inputLine = in.readLine()) != null)
+    while ((inputLine = in.readLine()) != null) {
       System.out.println(inputLine);
+    }
+
     in.close();
   }
 }
