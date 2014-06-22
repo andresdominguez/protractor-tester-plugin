235179924400d894733572809f345b1c21f6c4c7
andres <code@karateca.com>
2014-06-22 16:53:46 -0400

    split and the look for quotes

--- src/com/karateca/protractor/LocatorTester.java
+++ src/com/karateca/protractor/LocatorTester.java
@@ -17,12 +17,18 @@ public class LocatorTester {
     String resultsPattern = "(\\{\"results\":\\{)(.+)(\\}\\}$)";
     String keyAndValue = json.replaceAll(resultsPattern, "$2");
 
-    // The key / value looks like this:
+    // The key / value looks like this. Split at ":
     // "element.all(by.model('yourName')).count()":1
-    String keyValuePattern = "(\")(.+)(\":)(\"?([^\"]*)\"?)";
+    int i = keyAndValue.indexOf("\":") + 1;
+    if (i == 0) {
+      return null;
+    }
+
+    // Get the contents between quotes.
+    String quotesPattern = "\"?(.*?)\"?";
     return new Pair<String, String>(
-        keyAndValue.replaceAll(keyValuePattern, "$2"),
-        keyAndValue.replaceAll(keyValuePattern, "$5")
+        keyAndValue.substring(0, i).replaceAll(quotesPattern, "$1"),
+        keyAndValue.substring(i + 1).replaceAll(quotesPattern, "$1")
     );
   }
 }
