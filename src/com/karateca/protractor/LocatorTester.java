f00d2594675c3bea1a86326e132a782830d3478b
andres <code@karateca.com>
2014-06-21 23:02:58 -0400

    new

---
+++ src/com/karateca/protractor/LocatorTester.java
@@ -0,0 +1,13 @@
+package com.karateca.protractor;
+
+public class LocatorTester {
+  final JsonReader jsonReader;
+
+  public LocatorTester(JsonReader jsonReader) {
+    this.jsonReader = jsonReader;
+  }
+
+  public String testLocator(String locator) {
+    return null;
+  }
+}
