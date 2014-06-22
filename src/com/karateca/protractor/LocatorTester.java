22712bcc99bbbd8535afba7106ca16202d583100
andres <code@karateca.com>
2014-06-21 23:32:40 -0400

    inline constants

--- src/com/karateca/protractor/LocatorTester.java
+++ src/com/karateca/protractor/LocatorTester.java
@@ -4,8 +4,6 @@ import com.intellij.openapi.util.Pair;
 
 public class LocatorTester {
   final JsonReader jsonReader;
-  private static final String resultsPattern = "(\\{\"results\":\\{)(.+)(\\}\\}$)";
-  private static final String keyValuePattern = "(\")(.+)(\":\"?)(.+)(\")";
 
   public LocatorTester(JsonReader jsonReader) {
     this.jsonReader = jsonReader;
@@ -16,10 +14,12 @@ public class LocatorTester {
 
     // Parse the json string. It looks like this:
     // {"results":{"element.all(by.model('yourName')).count()":1}}
+    String resultsPattern = "(\\{\"results\":\\{)(.+)(\\}\\}$)";
     String keyAndValue = json.replaceAll(resultsPattern, "$2");
 
     // The key / value looks like this:
     // "element.all(by.model('yourName')).count()":1
+    String keyValuePattern = "(\")(.+)(\":\"?)(.+)(\")";
     return new Pair<String, String>(
         keyAndValue.replaceAll(keyValuePattern, "$2"),
         keyAndValue.replaceAll(keyValuePattern, "$4")
