a6168ea0cf792ba7e9ebbfd07a6df714c5b42bd8
andres <code@karateca.com>
2014-06-21 23:23:09 -0400

    Simplify code

--- src/com/karateca/protractor/LocatorTester.java
+++ src/com/karateca/protractor/LocatorTester.java
@@ -4,6 +4,8 @@ import com.intellij.openapi.util.Pair;
 
 public class LocatorTester {
   final JsonReader jsonReader;
+  private static final String resultsPattern = "(\\{\"results\":\\{)(.+)(\\}\\}$)";
+  private static final String keyValuePattern = "(\")(.+)(\":\"?)(.+)(\")";
 
   public LocatorTester(JsonReader jsonReader) {
     this.jsonReader = jsonReader;
@@ -12,14 +14,15 @@ public class LocatorTester {
   public Pair<String, String> testLocator(String locator) {
     String json = jsonReader.read(locator);
 
-    String pattern = "(\\{\"results\":\\{)(.+)(\\}\\}$)";
-    String aa = json.replaceAll(pattern, "$2");
+    // Parse the json string. It looks like this:
+    // {"results":{"element.all(by.model('yourName')).count()":1}}
+    String keyAndValue = json.replaceAll(resultsPattern, "$2");
 
+    // The key / value looks like this:
     // "element.all(by.model('yourName')).count()":1
-    pattern = "(\")(.+)(\":\"?)(.+)(\")";
-    String key = aa.replaceAll(pattern, "$2");
-    String value = aa.replaceAll(pattern, "$4");
-
-    return new Pair<String, String>(key, value);
+    return new Pair<String, String>(
+        keyAndValue.replaceAll(keyValuePattern, "$2"),
+        keyAndValue.replaceAll(keyValuePattern, "$4")
+    );
   }
 }
