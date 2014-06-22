a6a17a414e46feafdd477166e56285dbb815102b
andres <code@karateca.com>
2014-06-21 22:37:39 -0400

    Get key / value without gson

--- src/com/karateca/protractor/Tester.java
+++ src/com/karateca/protractor/Tester.java
@@ -1,5 +1,7 @@
 package com.karateca.protractor;
 
+import org.intellij.lang.annotations.Language;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -8,15 +10,13 @@ import java.net.URLEncoder;
 
 public class Tester {
 
-  private static final String baseUrl = "http://localhost:13000/testSelector?locators=";
+  private static final String baseUrl = "http://localhost:13000/testSelector?popupInput=";
 
   public static String testLocator(String locator) {
     StringBuilder sb = new StringBuilder();
 
     try {
-      String queryString = URLEncoder.encode(
-          String.format("{\"popupInput\":\"%s\"}", locator), "UTF-8");
-      String url = baseUrl + queryString;
+      String url = baseUrl + URLEncoder.encode(locator, "UTF-8");
       URLConnection connection = new URL(url).openConnection();
       BufferedReader in = new BufferedReader(
           new InputStreamReader(connection.getInputStream())
@@ -29,10 +29,30 @@ public class Tester {
       }
       in.close();
     } catch (Exception e) {
-      sb.append("Error testing locator")
-          .append(e.getMessage());
+      sb.append("Error testing locator").append(e.getMessage());
     }
 
     return sb.toString();
+  }
+
+  public static String gg(String locator) {
+    String json = testLocator(locator);
+
+    // Parse old school:
+    // {"results":{"element.all(by.model('yourName')).count()":1}}
+
+    // Remove results:
+    @Language("RegExp")
+    String pattern = "(\\{\"results\":\\{)(.+)(\\}\\}$)";
+    String aa = json.replaceAll(pattern, "$2");
+
+    // "element.all(by.model('yourName')).count()":1
+    pattern = "(\")(.+)(\":\"?)(.+)(\")";
+    String key = aa.replaceAll(pattern, "$2");
+    String value = aa.replaceAll(pattern, "$4");
+
+    System.out.println(key + value);
+
+    return null;
   }
 }
